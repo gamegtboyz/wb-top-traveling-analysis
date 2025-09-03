a3d9515e4aeed061206fd7fc604930ae1f10d614
Asa Bejraputra <gamegame.gtboyz@gmail.com>
2025-09-03 03:00:08 +0200

    add trend analysis and CAGR

--- streamlit-dashboard.py
+++ streamlit-dashboard.py
@@ -6,8 +6,7 @@ from scipy.stats import gmean
 
 # Page and title configuration
 st.set_page_config(page_title="International Tourism Analysis", page_icon='🌏', layout="wide")
-st.title("International Tourism Analysis (2000 - 2020)")
-
+st.title("International Tourism Analysis")
 # import data into work area
 data = pd.read_csv('./data/data.csv')
 data.dropna(axis=0,subset=['income_level'],inplace=True)
@@ -67,32 +66,157 @@ grouped_spendings['Decimals'] = grouped_spendings['rcpt_per_arvl'].map("$ {:,.2f
 grouped_contributions['Percentage'] = (grouped_contributions['rcpt_per_gdp']*100).map("{:.2f}%".format)
 
 # calculate the value change
-timed_grouped_receipt = grouped_receipt[(grouped_receipt['country_name'] == selected_countries) & (grouped_receipt['year'] <= selected_year)].sort_values(by='year')
+timed_grouped_receipt = grouped_receipt[(grouped_receipt['country_name'] == selected_countries)].sort_values(by='year')
 timed_grouped_receipt['last_period'] = timed_grouped_receipt['ST.INT.RCPT.CD'].shift(1)
 timed_grouped_receipt['pct_change'] = timed_grouped_receipt['ST.INT.RCPT.CD']/timed_grouped_receipt['last_period']
 
-timed_grouped_arrivals = grouped_arrivals[(grouped_arrivals['country_name'] == selected_countries) & (grouped_arrivals['year'] <= selected_year)].sort_values(by='year')
+timed_grouped_arrivals = grouped_arrivals[(grouped_arrivals['country_name'] == selected_countries)].sort_values(by='year')
 timed_grouped_arrivals['last_period'] = timed_grouped_arrivals['ST.INT.ARVL'].shift(1)
 timed_grouped_arrivals['pct_change'] = timed_grouped_arrivals['ST.INT.ARVL']/timed_grouped_arrivals['last_period']
 
-timed_grouped_spendings = grouped_spendings[(grouped_spendings['country_name'] == selected_countries) & (grouped_spendings['year'] <= selected_year)].sort_values(by='year')
+timed_grouped_spendings = grouped_spendings[(grouped_spendings['country_name'] == selected_countries)].sort_values(by='year')
 timed_grouped_spendings['last_period'] = timed_grouped_spendings['rcpt_per_arvl'].shift(1)
 timed_grouped_spendings['pct_change'] = timed_grouped_spendings['rcpt_per_arvl']/timed_grouped_spendings['last_period']
 
+timed_grouped_contributions = grouped_contributions[(grouped_contributions['country_name'] == selected_countries)].sort_values(by='year')
+
 # second part: show the trend analysis of selected country
-st.header(f"How do {selected_countries} comes so far?")
-st.write(f"Here's how do {selected_countries} performed so far from 2000 to {selected_year}.")
-# 1. Receipts trends from 2000 to selected year
-fig = px.line(grouped_receipt[(grouped_receipt['country_name'] == selected_countries) & (grouped_receipt['year'] <= selected_year)].sort_values(by='year'),
+st.header(f"How does {selected_countries} perform so far?")
+st.write(f"Here's how do {selected_countries} performed so far from 2000 to 2020.")
+
+# 1. Receipts performance
+fig = px.line(timed_grouped_receipt,
               x='year', y='ST.INT.RCPT.CD')
 
+fig.update_traces(line_color='#44af69')
+
+fig.add_annotation(
+    x=timed_grouped_receipt['year'].min() + ((timed_grouped_receipt['year'].max() - timed_grouped_receipt['year'].min())*.15),
+    y=timed_grouped_receipt['ST.INT.RCPT.CD'].max()*0.85,
+    text=f"Growth rate: ${((gmean(timed_grouped_receipt['pct_change'][1:-1])-1)*100):,.2f}%",
+    font=dict(size=24, color="black"),
+    align="center",
+    bgcolor="rgba(255, 255, 255, 0.8)",
+    bordercolor="black",
+    borderwidth=1,
+)
+
+fig.update_layout(title=f"Historical receipt performance",
+                  title_subtitle = {
+                      'font': {
+                          'color': '#FF0000',
+                          'style': 'italic'
+                          },
+                      'text': '2020 is excluded from growth rate calculations'
+                  },
+                  yaxis={
+                      'title':{
+                          'text': 'International Tourism Receipt (US Dollars)'
+                      }
+                  },
+                  xaxis={
+                      'title':{
+                          'text': 'Year'
+                      }
+                  })
+
 st.plotly_chart(fig, use_container_width=True)
-st.write(f"geogetric mean: {((gmean([1, 1.1, 1.21])-1)*100):,.0f}%")
-# 2. Arrivals trends from 2000 to selected year
 
-# 3. Spendings trends from 2000 to selected year
+# 2. Arrivals performance
+fig = px.line(timed_grouped_arrivals,
+              x='year', y='ST.INT.ARVL')
 
-# 4. GDP contributions from 2000 to selected year
+fig.update_traces(line_color='#2b9eb3')
+
+fig.add_annotation(
+    x=timed_grouped_arrivals['year'].min() + ((timed_grouped_arrivals['year'].max() - timed_grouped_arrivals['year'].min())*.15),
+    y=timed_grouped_arrivals['ST.INT.ARVL'].max()*0.85,
+    text=f"Growth rate: ${((gmean(timed_grouped_arrivals['pct_change'][1:-1])-1)*100):,.2f}%",
+    font=dict(size=24, color="black"),
+    align="center",
+    bgcolor="rgba(255, 255, 255, 0.8)",
+    bordercolor="black",
+    borderwidth=1,
+)
+
+fig.update_layout(title=f"Historical International Tourist Arrivals",
+                  title_subtitle = {
+                      'font': {
+                          'color': '#FF0000',
+                          'style': 'italic'
+                          },
+                      'text': '2020 is excluded from growth rate calculations'
+                  },
+                  yaxis={
+                      'title':{
+                          'text': 'International Tourist Arrivals (Person)'
+                      }
+                  },
+                  xaxis={
+                      'title':{
+                          'text': 'Year'
+                      }
+                  })
+
+st.plotly_chart(fig, use_container_width=True)
+
+# 3. Spendings trends
+fig = px.line(timed_grouped_spendings,
+              x='year', y='rcpt_per_arvl')
+
+fig.update_traces(line_color='#f1c40f')
+
+fig.add_annotation(
+    x=timed_grouped_spendings['year'].min() + ((timed_grouped_spendings['year'].max() - timed_grouped_spendings['year'].min())*.15),
+    y=timed_grouped_spendings['rcpt_per_arvl'].max()*0.85,
+    text=f"Growth rate: ${((gmean(timed_grouped_spendings['pct_change'][1:-1])-1)*100):,.2f}%",
+    font=dict(size=24, color="black"),
+    align="center",
+    bgcolor="rgba(255, 255, 255, 0.8)",
+    bordercolor="black",
+    borderwidth=1,
+)
+
+fig.update_layout(title=f"Historical International Tourist Spendings",
+                  title_subtitle = {
+                      'font': {
+                          'color': '#FF0000',
+                          'style': 'italic'
+                          },
+                      'text': '2020 is excluded from growth rate calculations'
+                  },
+                  yaxis={
+                      'title':{
+                          'text': 'International Tourist Arrivals Spendings (US Dollars)'
+                      }
+                  },
+                  xaxis={
+                      'title':{
+                          'text': 'Year'
+                      }
+                  })
+
+st.plotly_chart(fig, use_container_width=True)
+
+# 4. Historical GDP contributions
+fig = px.line(timed_grouped_contributions,
+              x='year', y='rcpt_per_gdp')
+
+fig.update_traces(line_color='#d90368')
+
+fig.update_layout(title=f"Historical International Tourist Spendings",
+                  yaxis={
+                      'title':{
+                          'text': 'International Tourist Arrivals Spendings (US Dollars)'
+                      }
+                  },
+                  xaxis={
+                      'title':{
+                          'text': 'Year'
+                      }
+                  })
+
+st.plotly_chart(fig, use_container_width=True)
 
 
 
